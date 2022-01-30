f55da8e31889f610451505334841c9d68422daa1
Rallie21m <rallie21m@yahoo.com>
2022-01-30 15:46:28 +0200

    Homework final

--- src/main/java/qa/automation/MyFirstHomework.java
+++ src/main/java/qa/automation/MyFirstHomework.java
@@ -1,5 +1,25 @@
 package qa.automation;
 
+import java.util.Scanner;
+
 public class MyFirstHomework {
+
+    public static void main(String[] args) {
+        System.out.println("PLEASE ENTER YOUR FIRST NAME:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("PLEASE ENTER YOUR FAVOURITE HOBBY:");
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        String yourHobby = scanner1.next();
+
+        System.out.println("Your name is " + firstName +
+                ". Your favourite hobby is " + yourHobby);
+
+    }
 
 }
